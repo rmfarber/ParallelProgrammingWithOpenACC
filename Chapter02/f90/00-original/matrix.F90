d64d9ecc83043a5944cb742a64471e131fc728d0
Jeff Larkin <jlarkin@nvidia.com>
2017-02-15 15:37:02 -0500

    Fixed mistake in CG example for Fortran.

--- Chapter02/f90/00-original/matrix.F90
+++ Chapter02/f90/00-original/matrix.F90
@@ -29,7 +29,7 @@ module matrix_mod
   subroutine allocate_3d_poisson_matrix(a, n)
     implicit none
     type(matrix) :: a
-    integer      :: n, num_rows, nnz, offsets(27), &
+    integer      :: n, nn, `num_rows, nnz, offsets(27), &
                     zstride, ystride, idx,         &
                     i, j, x, y, z
     real(8)      :: coefs(27)
@@ -69,9 +69,9 @@ module matrix_mod
     do i=1,num_rows
       arow_offsets(i) = idx
       do j=1,27
-        n=i+offsets(j)
+        nn=i+offsets(j)
         if ((n.ge.1).and.(n.le.num_rows)) then
-          a%cols(idx) = n
+          a%cols(idx) = nn
           a%coefs(idx) = coefs(j)
           idx = idx + 1
         endif
